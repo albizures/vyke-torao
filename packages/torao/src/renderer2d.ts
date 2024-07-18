7213a51bced83b337936ce2344d2328cf50a485a
Jose Albizures <jose@albizures.com>
2024-07-17 23:03:16 -0600

    feat: use a buffer canvas

--- packages/torao/src/renderer2d.ts
+++ packages/torao/src/renderer2d.ts
@@ -9,10 +9,11 @@ const _sola = rootSola.withTag('renderer2d')
 
 export function createRenderer2d(): Renderer {
 	let context: CanvasRenderingContext2D
+	const buffer = document.createElement('canvas').getContext('2d')!
 
 	return {
 		render(entities: Set<Entity>) {
-			context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+			buffer.clearRect(0, 0, context.canvas.width, context.canvas.height)
 			for (const entity of entities) {
 				const pos = entity.getComponent(positionComp)
 				const tex = entity.getComponent(textureComp)
@@ -21,7 +22,7 @@ export function createRenderer2d(): Renderer {
 					const { asset, atlas } = tex
 					if (asset.type === AssetType.Image || asset.type === AssetType.Canvas) {
 						if (asset.status === AssetStatus.Error) {
-							context.drawImage(asset.fallback(atlas.region).canvas,
+							buffer.drawImage(asset.fallback(atlas.region).canvas,
 								atlas.region.x, atlas.region.y,
 								atlas.region.width, atlas.region.height,
 								pos.x, pos.y,
@@ -29,7 +30,7 @@ export function createRenderer2d(): Renderer {
 							)
 							continue
 						}
-						context.drawImage(asset.use(),
+						buffer.drawImage(asset.use(),
 							atlas.region.x, atlas.region.y,
 							atlas.region.width, atlas.region.height,
 							pos.x, pos.y,
@@ -38,14 +39,17 @@ export function createRenderer2d(): Renderer {
 					}
 				}
 			}
+
+			context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+			context.drawImage(buffer.canvas, 0, 0)
 		},
 		setup(canvas: HTMLCanvasElement) {
 			const { width, height } = canvas
 
 			context = canvas.getContext('2d')!
 
-			canvas.width = width
-			canvas.height = height
+			buffer.canvas.width = width
+			buffer.canvas.height = height
 		},
 	}
 }
